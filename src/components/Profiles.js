9b362b8c96bfbf3030c17de357e610e744b3ce5a
Sreelekha Kutikanti <sreelekhakutikanti@gmail.com>
2022-08-19 19:08:18 +0530

    profile file added

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -6,18 +6,23 @@ export default function Profiles() {
    console.log(users)
   return (
     <div className='profiles-page'>
-        <h2>Users Profiles</h2>
-        <ul className='profiles'>
-            {users.map(each=>(
-                <Link to={`${each.user}`} className="profile-link">
-                    <li className="profile-container">
-                        <p><span className="profile-logo">{each.fname.charAt(0)}{each.lname.charAt(0)}</span></p>
-                        <h3>{each.user}</h3>
-                    </li>
-                </Link>
-                
-            ))}
-        </ul>
+        {users.length===0?<h2>No Profiles Found</h2>:(
+              <>
+              <h2>Users Profiles</h2>
+              <ul className='profiles'>
+                  {users.map(each=>(
+                      <Link to={`${each.user}`} className="profile-link">
+                          <li className="profile-container">
+                              <p><span className="profile-logo">{each.fname.charAt(0)}{each.lname.charAt(0)}</span></p>
+                              <h3>{each.user}</h3>
+                          </li>
+                      </Link>
+                      
+                  ))}
+              </ul>
+              </>
+        )}
+      
     </div>
   )
 }
